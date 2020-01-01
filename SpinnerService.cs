e9c5534003e2f3b4548be0fcc5a95419a5704ad7
Bipin Paul <iambipinpaul@outlook.com>
2020-01-02 03:18:24 +0545

    Add project files.

---
+++ SpinnerService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDisplaySpinnerAutomatically
+{
+	public class SpinnerService
+	{
+		public event Action OnShow;
+		public event Action OnHide;
+
+		public void Show()
+		{
+			OnShow?.Invoke();
+		}
+
+		public void Hide()
+		{
+			OnHide?.Invoke();
+		}
+	}
+}
