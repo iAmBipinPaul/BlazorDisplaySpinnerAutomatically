f886e809312d06b3384b29e09e886cd0f8a7cfab
Bipin Paul <iambipinpaul@outlook.com>
2020-07-26 23:48:06 +0545

    update to RTM Blaozr 3.2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorDisplaySpinnerAutomatically
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<SpinnerService>();
+            builder.Services.AddScoped<BlazorDisplaySpinnerAutomaticallyHttpMessageHandler>();
+            builder.Services.AddScoped(s =>
+            {
+                var accessTokenHandler = s.GetRequiredService<BlazorDisplaySpinnerAutomaticallyHttpMessageHandler>();
+                accessTokenHandler.InnerHandler = new HttpClientHandler();
+                var uriHelper = s.GetRequiredService<NavigationManager>();
+                return new HttpClient(accessTokenHandler)
+                {
+                    BaseAddress = new Uri(uriHelper.BaseUri)
+                };
+            });
+            await builder.Build().RunAsync();
+        }
     }
 }
